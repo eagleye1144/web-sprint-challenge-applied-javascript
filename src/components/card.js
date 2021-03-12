f8414578e798d2bce239e4315854bd5a8cb4092f
Mitchell Walker <eagleye1144@gmail.com>
2021-03-12 14:31:43 -0700

    all done

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +19,33 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const card = document.createElement('div')
+  const head = document.createElement('div')
+  const aut = document.createElement('div')
+  const imgCont = document.createElement('div')
+  const img = document.createElement('img')
+  const autName = document.createElement('span')
+
+  card.classList.add('card')
+  head.classList.add('headline')
+  aut.classList.add('author')
+  imgCont.classList.add('img-container')
+  img.src = `${article.authorPhoto}`
+
+  card.appendChild(head)
+  card.appendChild(aut)
+  aut.appendChild(imgCont)
+  imgCont.appendChild(img)
+  aut.appendChild(autName)
+
+  head.textContent = `${article.headline}`
+  autName.textContent= `${article.authorName}`
+
+  card.addEventListener('click', () =>{
+    console.log(`${article.headline}`)
+  })
+
+  return card
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +57,24 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  const cardContainer = document.querySelector(selector)
+  const myArr = ["javascript", "bootstrap", "technology", "jquery","node"];
+
+  axios.
+  get('https://lambda-times-api.herokuapp.com/articles')
+  .then((res) => {
+    myArr.forEach((article) =>{
+      res.data.articles[article].forEach(element =>{
+        const newCard = Card(element)
+        cardContainer.appendChild(newCard)
+      })
+
+    })
+
+  })
+  .catch((err) =>{
+    console.log('error!', err)
+  })
 }
 
 export { Card, cardAppender }
